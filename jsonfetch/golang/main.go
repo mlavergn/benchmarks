f20bd62b23d04320fbdcdde004f8d06496a8b0b4
Marc Lavergne <mlavergn@gmail.com>
2017-01-09 22:37:25 -0500

    Fix: go fmt

--- jsonfetch/golang/main.go
+++ jsonfetch/golang/main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-  "log"
-	"net/http"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"time"
 )
 
-func JSONTest(url string) (string) {
+func JSONTest(url string) string {
 	// perform the request
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("Content-Type", "application/json")
 	client := &http.Client{}
 	resp, _ := client.Do(req)
-	
+
 	// read the result
 	bytes, _ := ioutil.ReadAll(resp.Body)
 
 	// convert the JSON
 	var jsonMap map[string]interface{}
 	json.Unmarshal(bytes, &jsonMap)
-	
+
 	// return a string field
 	return jsonMap["ip"].(string)
 }
